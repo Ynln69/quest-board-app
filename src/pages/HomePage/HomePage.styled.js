142b44ff240381b915c99305f2078dfb81189d05
Yana <yanalunina96@gmail.com>
2023-08-03 23:29:37 +0300

    Create homePage styled

---
+++ src/pages/HomePage/HomePage.styled.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const HomeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+export const TitlePage = styled.h1`
+  margin: 0 auto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+  line-height: 18px;
+  letter-spacing: -0.28px;
+  width: 486px;
+`;
+
+export const LinkToCreate = styled.a`
+  color: #bedbb0;
+`;
