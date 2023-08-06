400cf97d6cd17b96c57575db6f628764212aa669
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-08-06 13:08:21 +0300

    редагування стейту і хендлерів для нового об'єкта юзера, який приходив на логінізацію

--- src/redux/auth/handlers.js
+++ src/redux/auth/handlers.js
@@ -1,9 +1,21 @@
-export const handleRegisterLoginFulfilled = (state, { payload }) => {
-  state.username = payload.user.username;
-  state.email = payload.user.email;
-  state.password = payload.user.password;
-  state.token = payload.user.token;
+export const handleRegisterFulfilled = (state, { payload: { user } }) => {
+  state.username = user.username;
+  state.email = user.email;
+  state.password = user.password;
+  state.token = user.token;
   state.isLoggedIn = true;
+};
+
+export const handleLoginFulfilled = (state, { payload }) => {
+  state.username = payload.username;
+  state.email = payload.email;
+  state.password = payload.password;
+  state.token = payload.token;
+  state.isLoggedIn = true;
+  state.id = payload._id;
+  state.boards = payload.boards;
+  state.theme = payload.theme;
+  state.avatarURL = payload.avatarURL;
 };
 
 export const handleLogOutFulfilled = state => {
