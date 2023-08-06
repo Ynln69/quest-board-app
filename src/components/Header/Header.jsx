09f7a0443f265053235897b7df6cf9000d9b28bc
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-08-06 19:09:18 +0300

    mobile sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,70 +18,93 @@ const Header = () => {
   const theme = useSelector(selectTheme);
   const [isOptionListOpen, setOptionListOpen] = useState(false);
   const [isSideBarOpen, setSideBarOpen] = useState(false);
-  const [isThemeSelectorOpen, setThemeSelectorOpen] = useState(false); 
+  const [isThemeSelectorOpen, setThemeSelectorOpen] = useState(false);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme');
     if (storedTheme) {
       dispatch(setTheme(storedTheme));
     }
-  }, [dispatch]);
+    let timeout;
 
-  
+    if (isSideBarOpen) {
+      timeout = setTimeout(() => {
+        document.addEventListener('click', handleOutsideClick);
+      }, 100);
+    } else {
+      document.removeEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [dispatch, isSideBarOpen]);
+
+  const handleOutsideClick = e => {
+    if (!e.target.closest('aside')) {
+      setSideBarOpen(false);
+    }
+  };
+
   const toggleOptionList = () => {
     setOptionListOpen(!isOptionListOpen);
   };
 
-  const sendThemeToBackend = (newTheme) => {
+  const sendThemeToBackend = newTheme => {
     localStorage.setItem('theme', newTheme);
   };
 
-  const onToggleTheme = (event) => {
+  const onToggleTheme = event => {
     const newTheme = event.target.value;
-  
+
     const themes = ['light', 'dark', 'violet'];
-    themes.forEach((t) => {
+    themes.forEach(t => {
       document.body.classList.remove(t);
     });
-  
+
     document.body.classList.remove(theme);
     document.body.classList.add(newTheme);
-  
+
     dispatch(setTheme(newTheme));
     sendThemeToBackend(newTheme);
   };
 
   const toggleSideBar = () => {
-    setSideBarOpen(!isSideBarOpen); 
+    setSideBarOpen(!isSideBarOpen);
   };
 
   const toggleThemeSelector = () => {
     setThemeSelectorOpen(!isThemeSelectorOpen);
   };
 
-   return (
+  return (
     <Container className={`theme-${theme}`}>
       <div onClick={toggleSideBar}>
         <MenuIcon className={`icon-menu theme-${theme}`} width="32" height="32">
           <use href={`${Sprite}#icon-menu`} />
         </MenuIcon>
       </div>
+      {isSideBarOpen && <Sidebar />}
       <HeaderWrap>
         <HeaderNav onClick={toggleOptionList} value={theme}>
           <div>
-          <SelectIcon className={`icon-chevron-down theme-${theme}`} onClick={toggleThemeSelector}>
+            <SelectIcon
+              className={`icon-chevron-down theme-${theme}`}
+              onClick={toggleThemeSelector}
+            >
               <use href={`${Sprite}#icon-chevron-down`} />
-              </SelectIcon>
+            </SelectIcon>
           </div>
           Theme
         </HeaderNav>
-        {isThemeSelectorOpen && ( 
-        <select value={theme} onChange={onToggleTheme}>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="violet">Violet</option>
-      </select>
-      )}
+        {isThemeSelectorOpen && (
+          <select value={theme} onChange={onToggleTheme}>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="violet">Violet</option>
+          </select>
+        )}
         <UserInfo />
       </HeaderWrap>
       {isSideBarOpen && <Sidebar theme={theme} isOpen={isSideBarOpen} />}
@@ -90,4 +113,3 @@ const Header = () => {
 };
 
 export default Header;
-
