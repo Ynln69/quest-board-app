961dd262e70d365f348cd0526c61f990892f3566
Inna Pokydko <inna.pokydko@gmail.com>
2023-08-08 23:53:25 +0300

    update theme

--- src/components/ThemeSelector/ThemeSelector.jsx
+++ src/components/ThemeSelector/ThemeSelector.jsx
@@ -1,46 +1,87 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTheme } from 'redux/theme/themeSlice';
-import { selectTheme } from 'redux/theme/selectors';
-import { updateTheme } from 'redux/theme/operations';
+import React, { useState, useEffect } from 'react';
+import {  useSelector, useDispatch } from 'react-redux';
+import { selectUser } from 'redux/auth/selectors';
+import { updateUser } from 'redux/auth/operations';
 import { Container, Select, Option } from './ThemeSelector.styled';
 
 const ThemeSelector = ({ onClose }) => {
+  const { theme } = useSelector(selectUser);
   const dispatch = useDispatch();
-  const theme = useSelector(selectTheme);
+  const [currentTheme, setCurrentTheme] = useState(theme);
 
-  const onToggleTheme = async event => {
+  useEffect(() => {
+    document.body.classList.remove('light', 'dark', 'violet');
+    document.body.classList.add(currentTheme);
+  }, [currentTheme]);
+
+  const onHandleTheme = async event => {
+    console.log(event);
     const newTheme = event.target.value;
 
-    const themes = ['light', 'dark', 'violet'];
-    themes.forEach(t => {
-      document.body.classList.remove(t);
-    });
-
-    document.body.classList.remove(theme);
-    document.body.classList.add(newTheme);
-
-    dispatch(setTheme(newTheme));
+    setCurrentTheme(newTheme);
 
     try {
-      await dispatch(updateTheme({ theme: newTheme }));
-      localStorage.setItem('theme', newTheme);
+      await dispatch(updateUser({ theme: newTheme }));
+      onClose();
     } catch (error) {
-      console.log('Error updating theme:', error);
+      console.error('Failed to update theme:', error);
     }
-    onClose();
   };
 
   return (
-    <Container >
-      <Select value={theme} onChange={onToggleTheme} >
-        
+    <Container>
+      <Select value={currentTheme} onChange={onHandleTheme}>
         <Option value="light">Light</Option>
         <Option value="dark">Dark</Option>
         <Option value="violet">Violet</Option>
-      
-              </Select>
+      </Select>
     </Container>
   );
 };
 export default ThemeSelector;
+
+
+
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { updateUser } from 'redux/auth/operations';
+// import { selectTheme } from 'redux/theme/selectors';
+// import { updateTheme } from 'redux/theme/operations';
+// import { Container, Select, Option } from './ThemeSelector.styled';
+
+// const ThemeSelector = ({ onClose }) => {
+//   const dispatch = useDispatch();
+//   const theme = useSelector(selectTheme);
+
+//   const onHandleTheme = async event => {
+//     const newTheme = event.target.value;
+
+//     const themes = ['light', 'dark', 'violet'];
+//     themes.forEach(t => {
+//       document.body.classList.remove(t);
+//     });
+
+//     document.body.classList.remove(theme);
+//     document.body.classList.add(newTheme);
+
+//         try {
+//       await dispatch(updateTheme({ theme: newTheme }));
+//       localStorage.setItem('theme', newTheme);
+//     } catch (error) {
+//       console.log('Error updating theme:', error);
+//     }
+//     dispatch(updateUser({ theme }));
+//     onClose();
+//   };
+
+//   return (
+//     <Container>
+//       <Select value={theme} onChange={onHandleTheme}>
+//         <Option value="light">Light</Option>
+//         <Option value="dark">Dark</Option>
+//         <Option value="violet">Violet</Option>
+//       </Select>
+//     </Container>
+//   );
+// };
+// export default ThemeSelector;
