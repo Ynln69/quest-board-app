eb36c3ed82686bf5d2e431dce6a4b31c01580342
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-08-13 18:54:09 +0300

    board name, sidebar style

--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react';
 import { HeadContainer, HeadText } from './HeaderDashboard.styled';
 import FilterModal from '../Filter/FilterModal';
 import sprite from '../../images/sprite.svg';
+import { useParams } from 'react-router-dom';
 
 export const HeaderDashboard = () => {
   const [isShowModal, setIsShowModal] = useState(false);
-
+  const { boardName } = useParams();
   const toogleShowModal = () => {
     setIsShowModal(!isShowModal);
   };
 
   return (
     <HeadContainer>
-      <HeadText>Name Board</HeadText>
+      <HeadText>{boardName}</HeadText>
       <button type="button" name="help" onClick={toogleShowModal}>
         <svg width={16} height={16}>
           <use href={`${sprite}#icon-filter`} />
