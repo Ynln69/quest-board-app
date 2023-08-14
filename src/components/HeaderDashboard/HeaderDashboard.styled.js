15414c1abc7fffa50a94f64fad99ff83da54f6a6
Olga007 <o.piankova@ukr.net>
2023-08-14 13:05:46 +0300

    filter styling
    Delete persist, styling filter

--- src/components/HeaderDashboard/HeaderDashboard.styled.js
+++ src/components/HeaderDashboard/HeaderDashboard.styled.js
@@ -16,10 +16,24 @@ export const HeadText = styled.p`
   letter-spacing: -0.36px;
 `;
 
-export const Button = styled.div`
+export const FilterBtnWrapper = styled.div`
+display: flex;
+gap: 8px;
+`;
+
+export const Button = styled.button`
   display: flex;
   align-items: center;
-  background-color: transparent;
+  
+  padding: 0%;
   border: none;
+
+  color: var(--board_main_color);
+  background-color: transparent;
+`;
+
+export const FilterSvg = styled.svg`
+  stroke: var(--chevron-down);
+  fill: transparent;
 
 `
